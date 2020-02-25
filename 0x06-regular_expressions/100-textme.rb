b5c3464df75c2abd205a045db2763a5eea2e8862
mlgomez0 <mlgomez230@hotmail.com>
2020-02-25 00:36:42 +0000

    Correction 100-flags

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(\+?\w+)\] \[to:(\+?\w+)\] \[flags:(\+?\w+)\]/).join(",")
+puts ARGV[0].scan(/\[from:(\+?\w+)\]\s\[to:(\+?\w+)\]\s\[flags:(-?[0-1]:-?[0-1]:-?[0-1]:-?[0-1]:-?[0-1])\]/).join(",")
