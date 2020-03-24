bb6681474d88be6db3be09afaf0854e54b0d6bca
mlgomez0 <mlgomez230@hotmail.com>
2020-03-24 14:14:39 +0000

    Adding comment to 4-

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,6 +1,7 @@
+#set up your client SSH configuration file
 class { 'ssh':
   storeconfigs_enabled => false,
-  server_options => {
+  server_options       => {
     'Match User www-data' => {
       'PasswordAuthentication' => 'no',
       'IdentityFile'           => '~/.ssh/holberton'
