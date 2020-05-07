05ed2443d4f48c1708b4e05d1c07d491b6ac8732
mlgomez0 <mlgomez230@hotmail.com>
2020-05-07 18:54:57 +0000

    create file

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""recursive function queries the Reddit API and returns all
+   hot posts for a given subreddit"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], v_aft=""):
+    """uses get request to reddit url to
+       get all hot posts """
+
+    h = {'user-agent': 'X-Modhash'}
+    q = {'after': v_aft}
+    url = "https://www.reddit.com/r/" + subreddit + "/hot.json"
+    get_req = requests.get(url, headers=h, params=q, allow_redirects=False)
+    pd = get_req.json()
+    if get_req.status_code == 404:
+        return (None)
+    else:
+        for post in pd["data"]["children"]:
+            hot_list.append(post["data"]["title"])
+        if v_aft is None:
+            return hot_list
+        else:
+            return (recurse(subreddit, hot_list, v_aft=pd["data"]["after"]))
