ae32dad166eea0f82f5f6359f4c05f7e4e557649
mlgomez0 <mlgomez230@hotmail.com>
2020-05-07 22:41:37 +0000

    create file

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""recursive function that queries the Reddit API, parses
+   the title of all hot articles, and prints a sorted
+   count of given keywords (case-insensitive,
+   delimited by spacesrecursive) """
+
+import requests
+
+
+def count_words(subreddit, word_list, v_aft=''):
+    """uses get request to reddit url to
+       get keyword in hot posts """
+
+    h = {'user-agent': 'X-Modhash'}
+    q = {'after': v_aft}
+    s1 = "https://www.reddit.com/r/"
+    word_list.sort()
+    for key in word_list:
+        url = s1 + subreddit + "/search.json?q=title:" + key + "&sort=hot"
+        get_req = requests.get(url, headers=h, params=q, allow_redirects=False)
+        pd = get_req.json()
+        if get_req.status_code == 404:
+            return None
+        count = 0
+        for post in pd["data"]["children"]:
+            count = count + post["data"]["title"].lower().count(key)
+        if count != 0:
+            print("{} : {}".format(key, count))
+"""
+            v_aft = pd["data"]["after"]
+            if v_aft is None:
+                if count != 0:
+                    print("{} : {}".format(key, count))
+            else:
+                return (count_words(subreddit, word_list, v_aft)) """
