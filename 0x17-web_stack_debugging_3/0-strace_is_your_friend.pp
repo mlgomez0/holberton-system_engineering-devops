b9ac25a5a15af24fc271639df21828eddc2e5534
mlgomez0 <mlgomez230@hotmail.com>
2020-05-20 16:47:46 +0000

    correct Puppets

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 #correct spelling error in word press settings file
 exec {'debugApache-WP':
   provider => shell,
-  command  => 'sed -i "s+require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );+require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );+g" /var/www/html/wp-settings.php',
+  command  => 'sed -i "s+require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );+require_once\
+	    ( ABSPATH . WPINC . \'/class-wp-locale.php\' );+g" /var/www/html/wp-settings.php',
 }
